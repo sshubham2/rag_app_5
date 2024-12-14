94a4e9ab32058413f9740d83fb4a0521a7cc54d1
Shubehndu Shubham <s2.shubh2@gmail.com>
2024-12-14 22:27:33 +0530

    update model lists

--- rag_app_5/src/config.py
+++ rag_app_5/src/config.py
@@ -7,27 +7,26 @@ class ModelProvider(Enum):
     GROQ = 'Groq'
 
 OPENAI_MODELS = {
-    'ChatGPT 4o Mini': 'gpt-4o-mini',
+    'ChatGPT 4o(Mini)': 'gpt-4o-mini',
     'ChatGPT 4o': 'gpt-4o',
-    'OpenAI o1-preview': 'o1-preview',
-    'OpenAI o1-mini': 'o1-mini'
+    'ChatGPT o1(Preview)': 'o1-preview-2024-09-12',
+    'ChatGPT o1(Mini)': 'o1-mini-2024-09-12'
 }
 
 ANTHROPIC_MODELS = {
-    'Claude 3.5 Sonnet': 'claude-3-5-sonnet-20240620',
+    'Claude 3.5 Sonnet': 'claude-3-5-sonnet-20241022',
     'Claude 3 Opus': 'claude-3-opus-20240229',
-    'Claude 3 Haiku': 'claude-3-haiku-20240307'
+    'Claude 3.5 Haiku': 'claude-3-5-haiku-20241022'
 }
 
 GROQ_MODELS = {
-    'Llama 3.1 70B (Preview)': 'llama-3.1-70b-versatile',
-    "Llama 3.1 8B (Preview)": "llama-3.1-8b-instant",
-    "Llama 3.2 90B (Preview)":"llama-3.2-90b-text-preview"
+    'Llama 3.3 70B': 'llama-3.3-70b-versatile'
 }
 
 MISTRAL_MODELS = {
     "Mistral NeMo": "open-mistral-nemo-2407",
-    "Mistral Large 2": "mistral-large-2407",
+    "Mistral Large 24.07": "mistral-large-2407",
+    "Mistral Medium 23.12": "mistral-medium-2312",
     "Mistral Small 24.09": "mistral-small-2409",
     "Codestral": "codestral-2405"
 }
